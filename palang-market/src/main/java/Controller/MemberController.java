dfbf1e80f9fdd63faea14faf984b3aa641676bd4
선진 <s22013@gsm.hs.kr>
2022-12-21 10:01:33 +0900

    commit

---
+++ palang-market/src/main/java/Controller/MemberController.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/java")
+public class MemberController {
+	
+}
